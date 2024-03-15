3b850a189f0d8e60352f24908bf1bc88060b688d
Annania Plossl <nia.plossl@gmail.com>
2024-03-15 12:12:55 -0400

    completed the lab

--- main.js
+++ main.js
@@ -3,7 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+document.addEventListener("click",event => {
+  console.log(event);
+  if(event.target.classList.contains("like-glyph")){
+    console.log("clicked on a heart");
+    if (event.target.classList.contains("activated-heart")) {
+      event.target.textContent = EMPTY_HEART;
+      event.target.classList.remove("activated-heart");
+    }
+    else{
+      mimicServerCall()
+      .then(()=>{
+        event.target.textContent = FULL_HEART;
+        event.target.classList.add("activated-heart");
+      })
+      .catch(message=>{
+        document.querySelector("#modal").classList.remove("hidden");
+        document.querySelector("#modal-message").textContent = message;
+        setTimeout(()=>document.querySelector("#modal").classList.add("hidden"),3000)
+      })
+    }
+  }
+})
 
 
 
